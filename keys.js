5de26fe3cbd3e5ea13688f9f60976b5156667f6c
Efe <efeowolide@gmail.com>
2018-12-15 23:05:35 -0600

    Liri is functional

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.07a67578850e400f842653c611e1753b,
-  secret: process.env.321caa4df6f44467b3a7309fb133c767
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
